97de1c48dd75278ec98b3f71254ddc48c4ca7a12
LCSW2507 <c0956644@mylambton.ca>
2025-04-07 10:31:12 -0400

    Delete Khattri TEch directory

--- Khattri TEch/Khattri TEch/js/main.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.getElementById('hamburger')
-const mainNav = document.getElementById('mainNav')
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active')
-  mainNav.classList.toggle('show')
-})
--- Khattri TEch/js/main.js
+++
@@ -1,8 +0,0 @@
-/* Student Name: Prabin Raj Karki
-Student ID: C0956644 */
-const hamburger = document.getElementById('hamburger')
-const mainNav = document.getElementById('mainNav')
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active')
-  mainNav.classList.toggle('show')
-})
